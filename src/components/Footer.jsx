bf5eb91a26abc21409dbb66319218956cc785202
Marcio Junior <marciofmjr2@gmail.com>
2024-09-21 19:50:05 -0300

    create banner home section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,14 @@
 function Footer() {
-  return <footer></footer>
+  return <>
+  <footer className="full-section bg-lime-900">
+    <div className="section-container">
+      <div>1</div>
+      <div>2</div>
+      <div>3</div>
+      <div>4</div>
+    </div>
+  </footer>
+  </>
 }
 
 export default Footer;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,11 @@
+import Banner from "../components/Banner";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 
 function Home() {
   return <>
     <Header />
-    <h1 className="text-4xl">Home</h1>
+    <Banner />
     <Footer />
   </>
 }
