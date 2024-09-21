f3b7929dc123dc30d4e07669f13a63de782057bf
Marcio Junior <marciofmjr2@gmail.com>
2024-09-21 20:37:19 -0300

    add nav to be responsive

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,14 +1,33 @@
 import { Link } from "react-router-dom";
 
+function NavLink(props) {
+  return (
+    <>
+      <li className="w-full flex lg:w-auto">
+        <Link
+          className="hover:text-yellow-400 hover:bg-lime-900 px-8 py-4 flex w-full lg:p-0 hover:lg:bg-transparent lg:w-fit lg:text-nowrap"
+          to={props.url}
+        >
+          {props.title}
+        </Link>
+      </li>
+    </>
+  );
+}
+
 function Nav() {
-  return <ul className="flex gap-12 font-bold text-slate-50">
-    <li><Link className="hover:text-yellow-400" to={`/`}>Home</Link></li>
-    <li><Link className="hover:text-yellow-400" to={`/about`}>About</Link></li>
-    <li><Link className="hover:text-yellow-400" to={`/menu`}>Menu</Link></li>
-    <li><Link className="hover:text-yellow-400" to={`/reservations`}>Reservations</Link></li>
-    <li><Link className="hover:text-yellow-400" to={`/order-online`}>Order Online</Link></li>
-    <li><Link className="hover:text-yellow-400" to={`/login`}>Login</Link></li>
-  </ul>
+  return (
+    <>
+      <ul className="absolute text-white flex flex-col top-24 left-0 w-full bg-lime-800 font-bold lg:flex-row lg:gap-12 lg:text-slate-50 lg:flex lg:relative lg:top-auto lg:left-auto lg:bg-transparent lg:w-auto">
+        <NavLink title="Home" url="/" />
+        <NavLink title="About" url="/about" />
+        <NavLink title="Menu" url="/menu" />
+        <NavLink title="Reservations" url="/reservations" />
+        <NavLink title="Order Online" url="/order-online" />
+        <NavLink title="Login" url="/login" />
+      </ul>
+    </>
+  );
 }
 
 export default Nav;
